afd901029c0305c8f6f895552850ef325b7755d8
yury <yury@tablet.com>
2020-03-25 01:20:40 +0300

    Set mappings specific to 4Good t103i 3g tablet

--- src/rotateinput.cpp
+++ src/rotateinput.cpp
@@ -33,7 +33,7 @@ struct InputProperty {
   Atom type;
   int format;
   QString name;
-  
+
   bool isRotationMatrix() const;
   void setRotationMatrix(const vector<float> &matrix);
 };
@@ -136,7 +136,7 @@ RotateInput::RotateInput(QObject* parent) : QObject{parent}, d{new Private}
       d->devices.push_back(device);
   }
   XIFreeDeviceInfo(deviceInfo);
-  
+
   for(auto device: d->devices) {
     for(auto property: device.properties()) {
       qDebug() << "Device" << device.name << ", property:" << property.name << ", atom=" << property.atom << ", type: " << property.type;
@@ -151,10 +151,10 @@ void RotateInput::rotate(Orientation orientation)
 {
 #ifdef USE_XINPUT
   static QHash<Orientation, QStringList>  orientation_matrix_map {
-    {TopUp, {"1", "0", "0", "0", "1", "0", "0", "0", "1"}},
-    {TopDown, {"-1", "0", "1", "0", "-1", "1", "0", "0", "1"}},
-    {LeftUp, {"0", "-1", "1", "1", "0", "0", "0", "0", "1"}},
-    {RightUp, {"0", "1", "0", "-1", "0", "1", "0", "0", "1"}},
+    {TopUp, {"0", "-1", "1", "1", "0", "0", "0", "0", "1"}},
+    {TopDown, {"0", "1", "0", "-1", "0", "1", "0", "0", "1"}},
+    {LeftUp, {"-1", "0", "1", "0", "-1", "1", "0", "0", "1"}},
+    {RightUp, {"1", "0", "0", "0", "1", "0", "0", "0", "1"}}
   };
   auto orientation_matrix = orientation_matrix_map[orientation];
   for(auto device: d->devices) {
@@ -162,10 +162,10 @@ void RotateInput::rotate(Orientation orientation)
   }
 #else
   static QHash<Orientation, vector<float>>  orientation_matrix_map {
-    {TopUp, {1, 0, 0, 0, 1, 0, 0, 0, 1}},
-    {TopDown, {-1, 0, 1, 0, -1, 1, 0, 0, 1}},
-    {LeftUp, {0, -1, 1, 1, 0, 0, 0, 0, 1}},
-    {RightUp, {0, 1, 0, -1, 0, 1, 0, 0, 1}},
+    {TopUp, {0, -1, 1, 1, 0, 0, 0, 0, 1}},
+    {TopDown, {0, 1, 0, -1, 0, 1, 0, 0, 1}},
+    {LeftUp, {-1, 0, 1, 0, -1, 1, 0, 0, 1}},
+    {RightUp, {1, 0, 0, 0, 1, 0, 0, 0, 1}}
   };
   auto orientation_matrix = orientation_matrix_map[orientation];
   qDebug() << "Setting rotation matrix: " << orientation_matrix;
